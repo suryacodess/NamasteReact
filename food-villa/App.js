81e5ce4d6c67d0042a5238d9657c36cd352fcdd8
suryacodess <helloosuryaprakash@gmail.com>
2023-07-21 11:32:39 +0530

    seting up food villa app

---
+++ food-villa/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const h1 = <h1>Food Villa</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(h1);
